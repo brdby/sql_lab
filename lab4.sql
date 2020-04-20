5fdd2830913272bd377de6afdb3c336c0d69c62b
Valery Kucherov <beardby@gmail.com>
2020-04-21 01:41:33 +0300

    lab 4 final fix (i hope)

--- lab4.sql
+++ lab4.sql
@@ -12,7 +12,15 @@ CREATE FUNCTION getLS(id SMALLINT) RETURNS SETOF LS_PSCH AS $$
     SELECT * FROM LS_PSCH WHERE DepartmentMembersID = id;
     $$ LANGUAGE SQL;
 
---Triggers
+--Test functions
+
+SELECT one();
+
+SELECT FireID, sum_fire_danger(FirePower, FireArea) FROM Fire;
+
+SELECT getLS(1);
+
+--Trigger
 CREATE TABLE LS_PSCH_log(
     Operation text,
     TimeOfOperation timestamp without time zone
@@ -25,15 +33,37 @@ CREATE FUNCTION LS_PSCH_logAction() RETURNS trigger AS $$
     END;
     $$ LANGUAGE plpgsql;
 
-CREATE FUNCTION Fire_id_changed() RETURNS trigger AS $$
-    BEGIN
-    RAISE NOTICE 'Fire ID was changed!';
-    END;
-    $$ LANGUAGE plpgsql;
-
 CREATE TRIGGER lspsch_modification
     AFTER INSERT OR UPDATE OR DELETE ON LS_PSCH
     EXECUTE PROCEDURE LS_PSCH_logAction();
+
+--Test trigger
+
+INSERT INTO LS_PSCH VALUES (18, 'Боширов А.А.', '01-01-1973', 18, 3, 6);
+DELETE FROM LS_PSCH WHERE EmployeeID = 18;
+
+SELECT * FROM LS_PSCH_log;
+
+--Trigger
+
+CREATE FUNCTION Fire_id_changed() RETURNS trigger AS $$
+    BEGIN
+    RAISE NOTICE 'Fire power was changed!';
+    END;
+    $$ LANGUAGE plpgsql;
+
+CREATE TRIGGER fire_update
+    AFTER UPDATE ON Fire
+    FOR EACH ROW
+    WHEN (OLD.FirePower IS DISTINCT FROM NEW.FirePower)
+    EXECUTE PROCEDURE Fire_power_changed();
+
+--Test trigger
+
+UPDATE Fire SET FireID=10  WHERE FireID=3;
+UPDATE Fire SET FirePower=1  WHERE FireID=3;
+
+--Trigger
 
 CREATE OR REPLACE FUNCTION forbid_more_than() RETURNS trigger
     LANGUAGE plpgsql AS
@@ -55,11 +85,16 @@ CREATE TRIGGER forbid_more_than_1
     FOR EACH STATEMENT
     EXECUTE PROCEDURE forbid_more_than(1);
 
-CREATE TRIGGER fire_update
-    BEFORE UPDATE ON Fire
-    FOR EACH ROW
-    WHEN (OLD.FireID IS DISTINCT FROM NEW.FireID)
-    EXECUTE PROCEDURE Fire_id_changed();
+--Test trigger
+
+INSERT INTO PSCH
+    VALUES  (11, 'Крутая', 'Первая верхняя 3А',  '74955553535', 'Иванов В.В.', 'Первый')
+            (12, 'Крутая', 'Первая верхняя 3А',  '74955553535', 'Иванов В.В.', 'Первый')
+            (13, 'Крутая', 'Первая верхняя 3А',  '74955553535', 'Иванов В.В.', 'Первый')
+            (14, 'Крутая', 'Первая верхняя 3А',  '74955553535', 'Иванов В.В.', 'Первый')
+            (15, 'Крутая', 'Первая верхняя 3А',  '74955553535', 'Иванов В.В.', 'Первый');
+
+DELETE FROM PSCH WHERE PSCHID>10;
 
 --Event trigger
 
@@ -75,6 +110,13 @@ CREATE OR REPLACE FUNCTION abort_any_command()
 CREATE EVENT TRIGGER abort_ddl ON ddl_command_start
    EXECUTE PROCEDURE abort_any_command();
 
+--Trigger Test
+
+CREATE TABLE aaa
+(
+    bbb SMALLINT
+);
+
 --Drop unnecessary
 DROP EVENT TRIGGER IF EXISTS abort_ddl;
 DROP TRIGGER IF EXISTS lspsch_modification ON LS_PSCH;
@@ -88,3 +130,5 @@ DROP FUNCTION IF EXISTS Fire_id_changed();
 DROP FUNCTION IF EXISTS forbid_more_than();
 DROP FUNCTION IF EXISTS abort_any_command();
 DROP TABLE LS_PSCH_log;
+
+DELETE FROM PSCH WHERE PSCHID>10;
